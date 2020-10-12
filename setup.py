fa6942c974836604f622ab4c14c5fd804c9b6818
Paul Scherer <pms69@cam.ac.uk>
2020-10-12 17:02:24 +0100

    [Setup] Setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(name="protclus",
+    version="0.7.0",
+    description="protclus: Library for protein complex discovery on PPI networks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/paulmorio/protclus",
+    author="Paul Scherer",
+    author_email="paul.geo2dr@gmail.com",
+    license="MIT",
+    install_requires=['numpy', 'tqdm', 'networkx'],
+    
+    test_suite="nose.collector",
+    tests_require=["nose", "nose-cover3"],
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=['Topic :: Scientific/Engineering :: Artificial Intelligence',
+                 'License :: OSI Approved :: MIT License',
+                 'Natural Language :: English',
+                 'Programming Language :: Python :: 3.6',
+                 'Operating System :: OS Independent'],
+    zip_safe=False
+    )
