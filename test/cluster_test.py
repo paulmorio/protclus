7458fad494ce998a67c0414ee7a4eeb5aeb423c1
paulmorio <pms69@cam.ac.uk>
2020-10-13 19:20:03 +0100

    MCODE and DPCLUS Unit tests

--- test/cluster_test.py
+++ test/cluster_test.py
@@ -1,4 +1,26 @@
 import numpy as np
 import networkx as nx
-from protclus import mcode
+from protclus import MCODE, DPCLUS
 
+unweighted_filename = "data/unweighted_example_network.txt"
+
+
+def test_mcode_cluster():
+    """
+    ## Testing MCODE on unweighted network
+    """
+    c = MCODE(unweighted_filename)
+    assert len(c.clusters) == 0
+    c.cluster()
+    assert len(c.clusters) == 59
+    assert len(c.clusters[-1]) == 4
+
+def test_dpclus_cluster():
+    """
+    ## Testing DPCLUS on unweighted network
+    """
+    c = DPCLUS(unweighted_filename)
+    assert len(c.clusters)==0
+    c.cluster()
+    assert len(c.clusters) == 901
+    assert len(c.clusters[-1]) == 2
