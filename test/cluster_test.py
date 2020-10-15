efddf25c8960c4b7f66a75e2d3a93be2c653ca24
paulmorio <pms69@cam.ac.uk>
2020-10-15 11:30:49 +0100

    [Testing] Test for IPCA class

--- test/cluster_test.py
+++ test/cluster_test.py
@@ -1,6 +1,6 @@
 import numpy as np
 import networkx as nx
-from protclus import MCODE, DPCLUS
+from protclus import MCODE, DPCLUS, IPCA
 
 unweighted_filename = "data/unweighted_example_network.txt"
 
@@ -24,3 +24,13 @@ def test_dpclus_cluster():
     c.cluster()
     assert len(c.clusters) == 901
     assert len(c.clusters[-1]) == 2
+
+def test_ipca_cluster():
+    """
+    ## Testing IPCA on unweighted network
+    """
+    c = IPCA(unweighted_filename)
+    assert len(c.clusters)==0
+    c.cluster()
+    assert len(c.clusters) == 2806
+    assert len(c.clusters[-1]) == 23
